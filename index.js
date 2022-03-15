adbb663b92582cde684d8ba3564ba540f129703c
Stan Persoons <Stan.persoons2@gmail.com>
2022-03-15 14:49:32 +0100

    More info from video

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use(express.static(join(__dirname, 'documentation')));
 
 app.get('/', async (req, res) => Home(req, res, __dirname));
 
-app.get('/:id', async (req, res) => Url(req, res, __dirname));
+app.get('/:id', async (req, res) => Url(req, res));
 
 app.listen(process.env.PORT || 3000);
--- routes/url/index.js
+++ routes/url/index.js
@@ -1,24 +1,26 @@
-import fetch from 'node-fetch';
-import { load } from 'cheerio';
+import updatedYoutubeInfo from 'updated-youtube-info';
 
 const Url = async (req, res) => {
 	try {
 		const id = req.params.id;
 		if (id === null || id === undefined) return;
 
-		let response = await fetch(`https://www.youtube.com/watch?v=${id}`);
-		let html = await response.text();
-
-		const $ = load(html);
-
-		let title = $('title').text();
-		title = title.substring(0, title.length - 10);
-
-		if (title.trim() !== '') {
-			res.status(200).json({ title, error: false });
-		} else {
-			throw new Error();
-		}
+		updatedYoutubeInfo(id, function (err, videoInfo) {
+			if (err) new Error(err);
+			let response = {
+				videoId: videoInfo.videoId,
+				url: videoInfo.url,
+				description: videoInfo.description,
+				creator: videoInfo.creator,
+				channelId: videoInfo.channelId,
+				thumbnailUrl: videoInfo.thumbnailUrl,
+				datePublished: videoInfo.datePublished,
+				genre: videoInfo.genre,
+				duration: videoInfo.duration,
+				views: videoInfo.views,
+			};
+			res.status(200).json({ ...response, error: false });
+		});
 	} catch (error) {
 		res.status(500).json({ error: true });
 	}
